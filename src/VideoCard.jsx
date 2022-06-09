cfdd6dab3f69172c8a1ebacf5eff058640399666
sharathnair9999 <sharathnair9999@gmail.com>
2022-06-09 23:43:25 +0530

    starter-files

--- src/VideoCard.jsx
+++ src/VideoCard.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import useClickOutside from './useClickOutside';
 
 const VideoCard = ({ video }) => {
   const { videoId, videoTitle, videoThumbnail } = video;
-  const { ref, isComponentVisible, setIsComponentVisible } =
-    useClickOutside(false);
+  const [showOptions, setShowOptions] = React.useState(false);
   return (
     <div key={videoId} className="video__card">
       <img src={videoThumbnail} alt="thumbnail1" />
       <div className="video_card__bottom">
         <p>{videoTitle}</p>
-        <section ref={ref} className="video__actions">
-          <button onClick={() => setIsComponentVisible((state) => !state)}>
+        <section className="video__actions">
+          <button onClick={() => setShowOptions((state) => !state)}>
             &#8942;
           </button>
           <section
-            className={`video__actions__options ${
-              !isComponentVisible && 'hidden'
-            } `}
+            className={`video__actions__options ${!showOptions && 'hidden'} `}
           >
-            {/* another fork */}
             <span>Like</span>
             <span>Add To Playlist</span>
             <span>Add To Watch Later</span>
